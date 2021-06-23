3531306e3cd94d009e016c2d565b9b82c712a58b
Jan Kowalski <perlon@op.pl>
2021-06-23 11:44:41 +0200

    Nodify tests

--- WyprawaNa8kPremiumXUnitTests/NumbersListTests.cs
+++ WyprawaNa8kPremiumXUnitTests/NumbersListTests.cs
@@ -14,7 +14,7 @@ namespace WyprawaNa8kPremiumTests
         [InlineData(false, -3, new int[] { 10, -15, 13, 7 })]
         [InlineData(true, 0, new int[] { 10, -2, -2, 2, 6, 8 })]
         [InlineData(false, 1, new int[] { 10, -2, -2, 2, 6, 8 })]
-        public void When_arraynumbers_contains_two_numbers_whouse_sum_is_k_NumbersIsSumExist01_should_be_return_expected_result(bool expectedResult, int k, int[] numbers)
+        public void When_arraynumbers_contains_two_numbers_whouse_sum_is_k_NumbersIsSumExist01_should_be_return_true_otherwise_false(bool expectedResult, int k, int[] numbers)
         {
             var numbersList = new NumbersList();
 
@@ -29,7 +29,7 @@ namespace WyprawaNa8kPremiumTests
         [InlineData(false, -3, new int[] { 10, -15, 13, 7 })]
         [InlineData(true, 0, new int[] { 10, -2, -2, 2, 6, 8 })]
         [InlineData(false, 1, new int[] { 10, -2, -2, 2, 6, 8 })]
-        public void When_arraynumbers_contains_two_numbers_whouse_sum_is_k_NumbersIsSumExist02_should_be_return_expected_result(bool expectedResult, int k, int[] numbers)
+        public void When_arraynumbers_contains_two_numbers_whouse_sum_is_k_NumbersIsSumExist02_should_be_return_true_otherwise_false(bool expectedResult, int k, int[] numbers)
         {
             var numbersList = new NumbersList();
 
@@ -45,7 +45,7 @@ namespace WyprawaNa8kPremiumTests
         [InlineData(false, -3, new int[] { 10, -15, 13, 7 })]
         [InlineData(true, 0, new int[] { 10, -2, -2, 2, 6, 8 })]
         [InlineData(false, 1, new int[] { 10, -2, -2, 2, 6, 8 })]
-        public void When_arraynumbers_contains_two_numbers_whouse_sum_is_k_NumbersIsSumExist03_should_be_return_expected_result(bool expectedResult, int k, int[] numbers)
+        public void When_arraynumbers_contains_two_numbers_whouse_sum_is_k_NumbersIsSumExist03_should_be_return_true_otherwise_false(bool expectedResult, int k, int[] numbers)
         {
             var numbersList = new NumbersList();
 
