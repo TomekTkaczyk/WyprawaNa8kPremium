feba5d7effb3eb146050c61a7bb74e7345b61976
devcode <devcode@unipromax.pl>
2021-07-10 00:05:01 +0200

    Zadanie10

--- WyprawaNa8kPremium/Thief.cs
+++ WyprawaNa8kPremium/Thief.cs
@@ -7,18 +7,18 @@ namespace WyprawaNa8kPremium
     {
         public int Rob(int[] houseValueArray)
         {
-            var (sumWithoutCurrentHouse, sumWithCurrentHouse) = GetRouteSummaryValue(houseValueArray.Length-1);
+            var (sumWithoutCurrentHouse, sumWithCurrentHouse) = GetRouteSummary(houseValueArray.Length-1);
 
             return Math.Max(sumWithoutCurrentHouse, sumWithCurrentHouse);
 
-            (int sumWithoutCurrentHouse, int sumWithCurrentHouse) GetRouteSummaryValue(int n)
+            (int sumWithoutCurrentHouse, int sumWithCurrentHouse) GetRouteSummary(int n)
             {
                 if(n == 0)
                 {
                     return (0, houseValueArray[0]);
                 }
 
-                var previous = GetRouteSummaryValue(n - 1);
+                var previous = GetRouteSummary(n - 1);
                 var maxPrevoius = Math.Max(previous.sumWithoutCurrentHouse, previous.sumWithCurrentHouse);
                 var maxCurrent = previous.sumWithoutCurrentHouse + houseValueArray[n];
 
