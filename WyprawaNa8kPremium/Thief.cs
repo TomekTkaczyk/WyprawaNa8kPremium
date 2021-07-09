b893647d2df71f984b1a226f7e9c594f24e4da38
devcode <devcode@unipromax.pl>
2021-07-10 00:03:22 +0200

    Zadanie10

---
+++ WyprawaNa8kPremium/Thief.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WyprawaNa8kPremium
+{
+    public class Thief
+    {
+        public int Rob(int[] houseValueArray)
+        {
+            var (sumWithoutCurrentHouse, sumWithCurrentHouse) = GetRouteSummaryValue(houseValueArray.Length-1);
+
+            return Math.Max(sumWithoutCurrentHouse, sumWithCurrentHouse);
+
+            (int sumWithoutCurrentHouse, int sumWithCurrentHouse) GetRouteSummaryValue(int n)
+            {
+                if(n == 0)
+                {
+                    return (0, houseValueArray[0]);
+                }
+
+                var previous = GetRouteSummaryValue(n - 1);
+                var maxPrevoius = Math.Max(previous.sumWithoutCurrentHouse, previous.sumWithCurrentHouse);
+                var maxCurrent = previous.sumWithoutCurrentHouse + houseValueArray[n];
+
+                return (maxPrevoius, maxCurrent);
+            }
+        }
+    }
+}
---
+++ WyprawaNa8kPremiumXUnitTests/ThiefTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WyprawaNa8kPremium;
+using Xunit;
+
+namespace WyprawaNa8kPremiumXUnitTests
+{
+    public class ThiefTests
+    {
+        [Theory]
+        [InlineData(4, new int[] {1, 2, 3, 1})]
+        [InlineData(12, new int[] { 2, 7, 9, 3, 1 })]
+        public void Rob_resturn_expected_value(int expectedMaximumValue, int[] valueArray)
+        {
+            var thief = new Thief();
+
+            Assert.Equal(expectedMaximumValue, thief.Rob(valueArray));
+        }
+    }
+}
