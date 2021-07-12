908e6e905e82493f31afc4e24299595534df1c40
devcode <devcode@unipromax.pl>
2021-07-12 23:40:39 +0200

    Zadanie11

--- WyprawaNa8kPremium/Nokia3310.cs
+++ WyprawaNa8kPremium/Nokia3310.cs
@@ -6,5 +6,50 @@ namespace WyprawaNa8kPremium
 {
     public class Nokia3310
     {
+        public string[] LetterCombinations(string digits)
+        {
+            string result = string.Empty;
+
+            var letters = new Dictionary<char, string>()
+            {
+                {'2', "abc"},
+                {'3', "def"},
+                {'4', "ghi"},
+                {'5', "jkl"},
+                {'6', "mno"},
+                {'7', "pqrs"},
+                {'8', "tuv"},
+                {'9', "wxyz"}
+            };
+
+            List<string> AddLetters(string digits)
+            {
+                var result = new List<string>();
+
+                if(digits.Length > 0)
+                {
+                    var temp = AddLetters(digits[1..]);
+                    for (var j = 0; j < letters[digits[0]].Length; j++)
+                    {
+                        var letter = letters[digits[0]][j];
+                        if (temp.Count == 0)
+                        {
+                            result.Add(letters[digits[0]][j].ToString());
+                        }
+                        else
+                        {
+                            for (var i = 0; i < temp.Count; i++)
+                            {
+                                result.Add(letters[digits[0]][j] + temp[i]);
+                            }
+                        }
+                    }
+                }
+
+                return result; 
+            }
+
+            return AddLetters(digits).ToArray();
+        }
     }
 }
--- WyprawaNa8kPremiumXUnitTests/Nokia3310Tests.cs
+++ WyprawaNa8kPremiumXUnitTests/Nokia3310Tests.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using WyprawaNa8kPremium;
 using Xunit;
 
 namespace WyprawaNa8kPremiumXUnitTests
@@ -8,9 +6,32 @@ namespace WyprawaNa8kPremiumXUnitTests
     public class Nokia3310Tests
     {
         [Fact]
-        public void LetterCombinations_should_by_return_valid_string_array()
+        public void LetterCombinations_should_by_return_array()
         {
+            var nokia = new Nokia3310();
 
+            Assert.IsType<string[]>(nokia.LetterCombinations(""));
+        }
+
+        [Theory]
+        [InlineData("232",
+            "ada", "adb", "adc", "aea", "aeb", "aec", "afa", "afb", "afc",
+            "bda", "bdb", "bdc", "bea", "beb", "bec", "bfa", "bfb", "bfc",
+            "cda", "cdb", "cdc", "cea", "ceb", "cec", "cfa", "cfb", "cfc"
+            )]
+        [InlineData("23", "ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf")]
+        [InlineData("24", "ag", "ah", "ai", "bg", "bh", "bi", "cg", "ch", "ci")]
+        [InlineData("27", "ap", "aq", "ar", "as", "bp", "bq", "br", "bs", "cp", "cq", "cr", "cs")]
+        public void LetterCombinations_should_by_return_expected_array(string digits, params string[] expected)
+        {
+            var nokia = new Nokia3310();
+            var result = nokia.LetterCombinations(digits);
+
+            Assert.Equal(expected.Length, result.Length);
+            for(var i = 0; i < expected.Length; i++)
+            {
+                Assert.True(expected[i].Equals( result[i] ));
+            }
         }
     }
 }
