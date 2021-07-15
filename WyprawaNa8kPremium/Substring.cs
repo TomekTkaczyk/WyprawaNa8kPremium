54faf1d322de5e6a7430d22872cb735ec947225d
devcode <devcode@unipromax.pl>
2021-07-15 19:12:22 +0200

    Zadanie12

--- WyprawaNa8kPremium/Substring.cs
+++ WyprawaNa8kPremium/Substring.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -33,6 +32,56 @@ namespace WyprawaNa8kPremium
             }
 
             return max;
+        }
+
+        public int LengthOfLongestSubstring2(string s)
+        {
+            if (s.Length == 0)
+            {
+                return 0;
+            }
+
+            if (!Regex.IsMatch(s, @"[ -~]"))
+            {
+                throw new ArgumentException("Bad argument");
+            }
+
+            if (s.Length == 1)
+            {
+                return 1;
+            }
+
+            var maxString = new StringBuilder();
+            int head = 0;
+            int tail = 0;
+
+            while(head < s.Length)
+            {
+                if (s[tail..head].Contains(s[head]))
+                {
+                    if (head - tail > maxString.Length)
+                    {
+                        maxString.Clear();
+                        maxString.Append(s[tail..head]);
+                    }
+                    tail += s[tail..head].IndexOf(s[head]) + 1;
+                }
+                head++;
+            }
+
+            head--;
+
+            if(!s[tail..head].Contains(s[head]))
+            {
+                head++;
+                if(head - tail > maxString.Length)
+                {
+                    maxString.Clear();
+                    maxString.Append(s[tail..head]);
+                }
+            }
+
+            return maxString.Length;
         }
     }
 }
--- WyprawaNa8kPremiumXUnitTests/TrainTests.cs
+++ WyprawaNa8kPremiumXUnitTests/TrainTests.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using WyprawaNa8kPremium;
 using Xunit;
 
 namespace WyprawaNa8kPremiumXUnitTests
-{    
+{
     public class TrainTests
     {
         [Theory]
@@ -14,6 +11,7 @@ namespace WyprawaNa8kPremiumXUnitTests
         [InlineData(1, "bbbbb")]
         [InlineData(3, "pwwkew")]
         [InlineData(7, "pwwkewsl gs")]
+        [InlineData(8, "pwwkewsl gh")]
         [InlineData(0, "")]
         public void LengthOfLongestSubstring_should_by_return_expected_value(int expected, string s)
         {
@@ -41,6 +39,42 @@ namespace WyprawaNa8kPremiumXUnitTests
             }
 
             Assert.Equal(96, train.LengthOfLongestSubstring(s.ToString()));
+        }
+
+
+        [Theory]
+        [InlineData(3, "abcabcbb")]
+        [InlineData(1, "bbbbb")]
+        [InlineData(3, "pwwkew")]
+        [InlineData(7, "pwwkewsl gs")]
+        [InlineData(8, "pwwkewsl gh")]
+        [InlineData(0, "")]                 
+        public void LengthOfLongestSubstring2_should_by_return_expected_value(int expected, string s)
+        {
+            var train = new Substring();
+
+            Assert.Equal(expected, train.LengthOfLongestSubstring2(s));
+        }
+
+        [Fact]
+        public void When_argument_is_out_of_valid_data_LengthOfLongestSubstring2_should_by_return_ArgumentEception()
+        {
+            var train = new Substring();
+
+            Assert.Throws<ArgumentException>(() => train.LengthOfLongestSubstring2("ą"));
+        }
+
+        [Fact]
+        public void When_argument_is_all_alphabet_LengthOfLongestSubstring2_should_by_return_96()
+        {
+            var train = new Substring();
+            var s = string.Empty;
+            for (var i = 32; i < 128; i++)
+            {
+                s += ((char)i).ToString();
+            }
+
+            Assert.Equal(96, train.LengthOfLongestSubstring2(s.ToString()));
         }
     }
 }
